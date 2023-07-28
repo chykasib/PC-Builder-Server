61cb29ee4f6f112b57f040d5e7bb6bd404466a9f
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2023-07-28 15:10:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rx4i6uo.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    const db = client.db("Pc-builder");
+    const productCollection = db.collection("products");
+
+    app.get("/products", async (req, res) => {
+      const product = await productCollection.find({}).toArray();
+      res.send({ message: "successful", status: 200, data: product });
+    });
+  } finally {
+  }
+};
+
+run().catch((err) => console.log(err));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
